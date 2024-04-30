3cb4c47a5d716a9ba53e416e45c5c2821635b774
AsylzhanTR <asylzantoleuhanov@gmail.com>
2024-04-30 19:52:25 +0600

    Configure setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="squirtle",
+    version="0.0.1",
+    url="https://github.com/AsylzhanTR/squirtle",
+
+    author="fathat",
+
+    description="SVG hook library for Pyglet",
+
+    my_modules=['squirtle'],
+    packages=['squirtle']
+)
